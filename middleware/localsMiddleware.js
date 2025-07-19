618382406101b309ce91221954c3c196488081eb
Brandon Bland <brandonbland90@gmail.com>
2025-07-19 10:45:00 -0400

    Added frontend folder for compiling SCSS and React/JS code

--- middleware/localsMiddleware.js
+++ middleware/localsMiddleware.js
@@ -1,3 +1,5 @@
+const { devMode } = require('../config/constants')
+
 const localsMiddleware = (req, res, next) => {
   res.locals.success = req.flash('success')
   res.locals.error = req.flash('error')
@@ -5,6 +7,7 @@ const localsMiddleware = (req, res, next) => {
   res.locals.info = req.flash('info')
   res.locals.errors = []
   res.locals.oldInput = {}
+  res.locals.devMode = devMode
   next()
 }
 
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ require('dotenv').config({ quiet: true })
 const { app } = require('./app')
 const port = process.env.PORT || 5000
 const { devMode } = require('./config/constants')
+const chalk = require('chalk')
 
 app.listen(port, () => {
   if (devMode) {
-    console.log(`Server listening on http://localhost:${port}`)
+    console.log(
+      chalk.blueBright(
+        `✈️  Express server listening on http://localhost:${port}`
+      )
+    )
   }
 })
